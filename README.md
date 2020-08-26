f8fdc4a1a3938e9507e4f65adae8f470ee89f745
Rahul Singh <rscodedocs@gmail.com>
2020-08-26 20:19:14 +0530

    finished performance

---
+++ README.md
@@ -0,0 +1,4 @@
+> Benchmarking the app
+>>cmd
+    1. ab
+    2. ab -c 50 -n 500 localhost:4000/fast
--- server/src/performance.js
+++ server/src/performance.js
@@ -3,35 +3,22 @@
 // i.e. running node js in cluster mode
 const express = require('express');
 
-// cluster module
-const cluster = require('cluster');
+const app = express();
 
-// check if cluster is master
-// console.log(cluster.isMaster);
-
-if (cluster.isMaster) {
-  // causes index.js to be executed in child mode
-  cluster.fork();
-  cluster.fork();
-} else {
-  // run all server code here - child mode
-  const app = express();
-
-  function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
-  }
-
-  app.get('/', (req, res) => {
-    doWork(5000); // 5sec
-    res.json({ message: 'Hello to the world' });
-  });
-
-  app.get('/fast', (req, res) => {
-    res.json({ message: 'Hello I am fast' });
-  });
-
-  app.listen(4000, () => {
-    console.log('I  am server which needs to improve the performance');
-  });
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
 }
+
+app.get('/', (req, res) => {
+  doWork(5000); // 5sec
+  res.json({ message: 'Hello to the world' });
+});
+
+app.get('/fast', (req, res) => {
+  res.json({ message: 'Hello I am fast' });
+});
+
+app.listen(4000, () => {
+  console.log('I  am server which needs to improve the performance');
+});
